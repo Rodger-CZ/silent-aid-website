95b84314eb690525b01a55250bed3a2630b5d195
Faustine Rodgers <faustinerodgers714@gmail.com>
2026-02-20 19:13:20 +0300

    Add mock functions for message display and submission

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  alert("Event closed (mock). Next step: connect this to your FastAPI backend.");
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Message sent (mock). Next step: enable Netlify Forms or connect a backend endpoint.");
+}
